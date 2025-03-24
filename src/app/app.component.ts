079c0c11ad307fa15b0cfd103c801567ad1394a0
Aarushi2146 <aarug2146@gmail.com>
2025-03-24 12:27:02 +0530

    UC5 regex added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,19 @@ export class AppComponent {
   logoUrl = 'assets/BridgeLabzlogo.png';
   url = "https://www.bridgelabz.com";
   userName: string = "";
+  nameError: string ="";
 
   onClick($event){
     console.log("Save button is clicked", $event);
     window.open(this.url, "_blank");
+  }
+  onInput($event){
+    console.log("Change event Occured!", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError ="";
+      return;
+    }
+    this.nameError ="Name is incorrect!";
   }
 }
